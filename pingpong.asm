0e4c2141c987957b2c46303e78486603a8869a09
MRehanArshad <f233098@cfd.nu.edu.pk>
2024-12-08 00:10:38 +0500

    Changing the logic for smooth working of program

--- pingpong.asm
+++ pingpong.asm
@@ -1,353 +1,294 @@
 [org 0x100]
 jmp start
-    pad1: dw 1762, 1922, 2082          ; value where pad1 should print
-    pad2: dw 1916, 2076, 2236          ; value where pad2 should print
-    ball: dw 2000                      ; value where ball will print
-    direction: dw 1                    ; for setting the direction of ball
-    leftCollision: db 0
-    RightCollision: db 0
-    UpCollision: db 0
-    DownCollision: db 0
-; ------------- Subroutine for clearing the screen ---------------
+; ---- Data Variables ----
+pad1: dw 1764, 1924, 2084          ; value where pad1 should print
+pad2: dw 1916, 2076, 2236          ; value where pad2 should print
+ballx: db 12
+bally: db 40
+ball: db 'O', 0
+toxpositive: db 0                  ; for x in positive flag 
+toypositive: db 1                  ; for x in negative flag 
+scoreL: db 0                       ; Score for left player
+scoreR: db 0                       ; Score for Right Player
+scorediL: dw 330                   ; di for score L 
+scorediR: dw 468                   ; di for score R 
+
+; ---- For Movement in the pads ----
+
+kbisr:        
+push ax 
+push es
+
+mov  ax, 0xb800 
+mov  es, ax             ; point es to video memory 
+in   al, 0x60           ; read a char from keyboard port 
+cmp  al, 0x11           ; is the key 'W'
+je Wkey
+cmp al, 0x1F            ; is the key 'S'
+je Skey
+cmp al, 0x48            ; is down arrow
+je UpArrowKey  
+cmp al, 0x50            ; is UP arrow
+je DownArrowKey
+
+nomatch:      
+mov  al, 0x20 
+out  0x20, al           ; send EOI to PIC 
+pop  es 
+pop  ax 
+iret
+
+Wkey:
+cmp word[pad1], 482
+jl nomatch
+sub word[pad1], 160
+sub word[pad1+2], 160
+sub word[pad1+4], 160
+jmp nomatch
+
+Skey:
+cmp word[pad1+4], 3520
+jg nomatch
+add word[pad1], 160
+add word[pad1+2], 160
+add word[pad1+4], 160
+jmp nomatch
+
+UpArrowKey:
+cmp word[pad2], 636
+jl nomatch
+sub word[pad2], 160
+sub word[pad2+2], 160
+sub word[pad2+4], 160
+jmp nomatch
+
+DownArrowKey:
+cmp word[pad2+4], 3518
+jg nomatch
+add word[pad2], 160
+add word[pad2+2], 160
+add word[pad2+4], 160
+jmp nomatch
+
+; ---- For clearing the Screen ----
 clrscr:
-    push bp             
-    mov bp, sp
-    pusha                   ; pushing all register into the stack
-    push es                 ; for storing the previous value of es
-    ; for setting the value of es
-    push 0xb800             
-    pop es
-    mov di, 0               ; setting the value of destination index
-    mov cx, 2000            ; setting the counter for the rep
-    mov ax, 0x0720          ; storing the I want to display on the console
-    rep stosw               ; for moving value of ax into es:di 
-    ; Restoring value of the register
-    pop es
-    popa
-    mov sp, bp
-    pop bp
-    ret
+push bp             
+mov bp, sp
+pusha                      ; pushing all register into the stack
+push es                    ; for storing the previous value of es
+; for setting the value of es
+push 0xb800             
+pop es
+mov di, 318
+mov dx, 21
+clrscr_loop:
+add di, 6                 ; setting the value of destination index
+mov cx, 77                ; setting the counter for the rep
+mov ax, 0x0720
+repne stosw
+dec dx
+jnz clrscr_loop
 
-; ------------ For Printing the Square -------------------
+; Restoring value of the register
+pop es
+popa
+mov sp, bp
+pop bp
+ret
 
-return_true2:
-    mov word[bp+4], 1
-    jmp square_condition_exit
-square_condition:
-    push bp
-    mov bp, sp
-    pusha
-    mov ax, [bp+10]
-    cmp [bp+8], ax
-    jz return_true2
-    cmp word[bp+8], 1
-    jz return_true2
-    cmp word[bp+6], 80
-    jz return_true2
-    cmp word[bp+6], 1
-    jz return_true2
-    mov word[bp+4], 0
-    square_condition_exit:
-    popa
-    mov sp, bp
-    pop bp
-    ret
+; ---- For Printing the Wall ----
+printWall:
+push bp
+mov bp, sp
+pusha
+push es
 
-printEstarik:
-    push 0xb800
-    pop es
-    mov ax, [bp+4]
-    mov word[es:di], ax         ; for printing an estarik
-    jmp square_loop_2_next
-square:
-    push bp
-    mov bp,sp
-    pusha
+push 0xb800
+pop es
+mov ax, 0xFF20
+mov di, 0
+mov cx, 2000
+rep stosw
 
-    mov si, 25   ; for storing the length of square
-    mov cx, 25          ; moving length of square
-    mov di, 0          ; moving the starting of di
+pop es
+popa
+mov sp, bp
+pop bp
+ret
 
+; ----- Different Case of Collision
+UpCollision:
+mov byte[toxpositive], 1
+jmp exit_checkCollision
+DownCollision:
+mov byte[toxpositive], 0
+jmp exit_checkCollision
+LeftCollision:
+mov byte[toypositive], 1
+jmp exit_checkCollision
+RightCollision:
+mov byte[toypositive], 0
+jmp exit_checkCollision
+; ----- For checking Collision with the balls -----
+checkCollision:
+push bp
 
-    square_loop_1:
-        mov dx, 80      ; moving the length of the square
-        square_loop_2:
-            mov ax, 0
-            push si
-            push cx
-            push dx
-            sub sp, 2
-            call square_condition
-            pop ax
-            pop dx
-            pop cx
-            pop si
-            cmp ax, 1
-            jz printEstarik
-            square_loop_2_next:
-            add di, 2
-            sub dx, 1
-            cmp dx, 0
-            jnz square_loop_2
-        ; shl si, 1
-        ; add di, 160
-        ; sub di, si
-        ; sub di, si
-        ; shr si, 1
-    loop square_loop_1
+cmp byte[ballx], 2
+jle UpCollision
+cmp byte[ballx], 22
+jge DownCollision
+cmp byte[bally], 2
+jle LeftCollision
+cmp byte[bally], 78
+jge RightCollision
+exit_checkCollision:
+pop bp
+ret
 
-    popa
-    mov sp, bp
-    pop bp
-    ret 2
+; ----- For Adding x and y ------
+xpositive:
+add byte[ballx], 1
+ret
+xnegative:
+sub byte[ballx], 1
+ret
+ypositive:
+add byte[bally], 2
+ret
+ynegative:
+sub byte[bally], 2
+ret
 
-; ------------- Subroutine for printing the pad --------------
-print_pads:
-    pusha
-    push es
-    ; setting the value of es register
-    push 0xb800
-    pop es
-    ; moving the value to print into ax
-    mov ax, 0x4F20
-    ; moving the address of array that contain the location of pad1 into bx
-    mov bx, pad1
-    ; printing pad1
-    mov di, [bx]
-    stosw
-    mov di, [bx+2]
-    stosw
-    mov di, [bx+4]
-    stosw
-    ; moving the address of array that contain the location of pad2 into bx
-    mov ax, 0x1F20
-    mov bx, pad2
-    ; printing pad2
-    mov di, [bx]
-    stosw
-    mov di, [bx+2]
-    stosw
-    mov di, [bx+4]
-    stosw
-    ; for restoring the value of registers
-    pop es
-    popa
-    ret
+Xispositive:
+call xpositive
+jmp Addxy_checky
+Yispositive:
+call ypositive
+jmp Addxy_exit
 
-; ---- Subroutine for checking the collision of the ball with any pad or wall ----
-Left:
-    mov byte[leftCollision], 1
-    mov byte[RightCollision], 0
-    mov byte[UpCollision], 0
-    mov byte[DownCollision], 0
-    jmp return_true
-Right:
-    mov byte[leftCollision], 0
-    mov byte[RightCollision], 1
-    mov byte[UpCollision], 0
-    mov byte[DownCollision], 0
-    jmp return_true
-Up:
-    mov byte[leftCollision], 0
-    mov byte[RightCollision], 0
-    mov byte[UpCollision], 1
-    mov byte[DownCollision], 0
-    jmp return_true
-Down:
-    mov byte[leftCollision], 0
-    mov byte[RightCollision], 0
-    mov byte[UpCollision], 0
-    mov byte[DownCollision], 1
-    jmp return_true
-return_true:
-    mov word[bp+4], 1
-    jmp exit_check_collision
-check_collision:
-    push bp
-    mov bp, sp
-    pusha               ; pushing value of all registers
-    push es
-    ; setting value of es 
-    push ds
-    pop es
-    mov di, pad1
-    mov ax, word[ball]  ; moving the value of ball into ax
-    mov cx, 3
-    repne scasw         ; for comparing the array of pad1 with ax
-    jz Left             ; jump to return true 
-    mov di, pad2
-    mov cx, 3
-    repne scasw         ; for comparing the array of pad2 with ax
-    jz Right
-    cmp ax, 320           ; for checking the collision with top wall
-    jle Up
-    cmp ax, 3680        ; for checking the collision with bottom wall
-    jge Down
-    mov dx, 0
-    sub ax, 156
-    mov bx, 160
-    div bx
-    cmp dx, 0
-    jz Right
-    mov dx, 0
-    mov ax, word[ball]
-    sub ax, 4
-    mov bx, 160
-    div bx
-    cmp dx, 0
-    jz Left
-    mov word[bp+4], 0       ; return false
-    exit_check_collision:
-    pop es
-    popa                ; restoring value of all registers
-    mov sp, bp
-    pop bp
-    ret
+Addxy:
+pusha
+cmp byte[toxpositive], 1
+je Xispositive
+call xnegative
+Addxy_checky:
+cmp byte[toypositive], 1
+je Yispositive
+call ynegative
+Addxy_exit: 
+popa
+ret
 
-; ------------ Subroutine for changing the direction of the ball ------------
-Novelty1:
-    cmp byte[RightCollision], 1
-    jnz setDirectionInc
-    mov word[direction], 4
-    jmp setDirection_exit
-Novelty2:
-    cmp byte[DownCollision], 1
-    jnz setDirectionInc
-    mov word[direction], 1
-    jmp setDirection_exit 
-Novelty3:
-    cmp byte[leftCollision], 1
-    jnz setDirectionInc
-    mov word[direction], 2
-    jmp setDirection_exit
-Novelty4:
-    cmp byte[UpCollision], 1
-    jnz for4
-    mov word[direction], 3
-    jmp setDirection_exit
-setDirection:
-    cmp word[direction], 1
-    jz Novelty1
-    cmp word[direction], 2
-    jz Novelty2
-    cmp word[direction], 3
-    jz Novelty3
-    cmp word[direction], 4
-    jz Novelty4
-    for4:
-        mov word[direction], 0
-    setDirectionInc:
-        add word[direction], 1
-    setDirection_exit:
-    jmp changDirection_exit
-changDirection:
-    push bp
-    mov bp, sp
-    pusha
+; ----- For updating the value of x and y -----
+updatexyForball:
+call checkCollision
+call Addxy
+ret
 
-    cmp word[bp+4], 1
-    jz setDirection
+; ----- For displaying the ball -----
+displayBall:
+pusha
+push es
 
-    changDirection_exit:
-    popa
-    mov sp, bp
-    pop bp
-    ret
+mov  ah, 0x13           ; service 13 - print string 
+mov  al, 1              ; subservice 01 – update cursor 
+mov  bh, 0              ; output on page 0 
+mov  bl, 7              ; normal attrib 
+mov  dh, byte[ballx]
+mov dl, byte[bally] 
+mov  cx, 1              ; length of string 
+push cs 
+pop  es                 ; segment of string  
+mov  bp, ball           ; offset of string  
+int  0x10               ; call BIOS video service 
 
-; ------------ Subroutine for printing the ball --------------------
-direction1:
-    push bp
-    mov bp, sp
-    sub word[ball], 156
-    sub sp, 2
-    call check_collision
-    call changDirection 
-    mov sp, bp
-    pop bp
-    jmp print_ball_exit
-direction2:
-    push bp
-    mov bp, sp
-    add word[ball], 164
-    sub sp, 2
-    call check_collision
-    call changDirection
-    mov sp, bp
-    pop bp
-    jmp print_ball_exit
-direction3:
-    push bp
-    mov bp, sp
-    add word[ball], 156
-    sub sp, 2
-    call check_collision
-    call changDirection
-    mov sp, bp
-    pop bp
-    jmp print_ball_exit
-direction4:
-    push bp
-    mov bp, sp
-    sub word[ball], 164
-    sub sp, 2
-    call check_collision
-    call changDirection
-    mov sp, bp
-    pop bp
-    jmp print_ball_exit
-print_ball:
-    push bp
-    mov bp, sp
-    pusha
-    push es
-    ; setting the value of es
-    push 0xb800
-    pop es
+call updatexyForball
 
-    mov bx, ball    ; moving the address of variable that contain the location of ball
-    mov di, [bx]    ; setting the value of di
-    mov ax, 0x0E4F  ; moving the value to print into ax
-    stosw           ; printing the ball on the screen
-    cmp word[direction], 1  
-    jz direction1
-    cmp word[direction], 2
-    jz direction2
-    cmp word[direction], 3
-    jz direction3
-    cmp word[direction], 4
-    jz direction4
-    ; defining a label for the exit point of print_ball subroutine
-    print_ball_exit:
-    pop es
-    popa
-    mov sp, bp
-    pop bp
-    ret
+pop es
+popa
+ret
 
-; --------- for Introducing some delay in the function ----------
+; ---- For displaying the pad ----
+displayPads:
+pusha
+push es
+
+push 0xb800                ; setting the value of es register
+pop es
+mov ax, 0x4F20             ; moving the value to print into ax
+mov bx, pad1               ; moving the address of array that contain the location of pad1 into bx 
+mov di, [bx]               ; printing pad1 
+stosw
+mov di, [bx+2]
+stosw
+mov di, [bx+4]
+stosw
+mov ax, 0x1F20             ; moving the address of array that contain the location of pad2 into bx 
+mov bx, pad2
+mov di, [bx]
+stosw
+mov di, [bx+2]
+stosw
+mov di, [bx+4]
+stosw
+
+pop es
+popa
+ret
+
+; ---- For displaying the Score -----
+displayScore:
+pusha
+push es
+
+push 0xb800
+pop es
+mov di, word[scorediL]  ; for pointing di to appropiate location
+mov ah, 0x07 
+mov al, byte[scoreL]
+add al, 0x30
+stosw
+mov di, word[scorediR]  ; for pointing di to appropiate location
+mov ah, 0x07
+mov al, byte[scoreR]
+add al, 0x30
+stosw
+pop es
+popa
+ret
+
+printBoard:
+call displayBall
+call displayPads
+call displayScore
+ret
+
+; ---- For adding some delay ----
 delay:
-    pusha
-    mov ax, 0
-    mov cx, 0xFFFF
-    delay_label:
-    inc ax
-    cmp ax, 0xFFFF
-    jnz delay_label
-    popa
-    ret
+pusha
+mov ax, 0
+mov cx, 0xFFFF
+delay_label:
+loop delay_label
+popa
+ret
 
-; ------------ Subroutine for printing the board --------------------
-print_board:
-    call clrscr             ; clearing the screen
-    push 0xFE20
-    call square
-    call print_pads         ; for printing the pads
-    call print_ball         ; for printing the ball
-    call delay              ; for introducing some delay
-    ret
+; ---- This is Start Label ----
 start:
-    call clrscr
-    call print_board
-    jmp start
+xor  ax, ax 
+mov  es, ax             ; point es to IVT base 
+cli                     ; disable interrupts 
+mov  word [es:9*4], kbisr ; store offset at n*4 
+mov  [es:9*4+2], cs     ; store segment at n*4+2 
+sti                     ; enable interrupts
+call printWall
+start_loop:
+call clrscr
+call printBoard
+call delay
+call delay
+call delay
+jmp start_loop
+
 mov ax, 0x4c00
 int 0x21
